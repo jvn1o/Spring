dfd9f5249d5d4c0a1f317323e199d839c04b4085
jvn1o <junok0987@naver.com>
2025-03-13 21:53:40 +0900

    250313

--- demo/src/main/java/hellospring/entity/Hello.java
+++ demo/src/main/java/hellospring/entity/Hello.java
@@ -1,7 +1,6 @@
 package hellospring.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +9,16 @@ import lombok.Setter;
 @Getter
 public class Hello {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "message", nullable = false)
+    private String message;
 }
