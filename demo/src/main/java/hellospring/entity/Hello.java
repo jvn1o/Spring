07131e04249682390d1e18cc1d9946d2729ae4eb
jvn1o <junok0987@naver.com>
2025-03-13 21:49:24 +0900

    250313

--- demo/src/main/java/hellospring/entity/Hello.java
+++ demo/src/main/java/hellospring/entity/Hello.java
@@ -1,7 +1,14 @@
 package hellospring.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Hello {
+    @Id
+    private Long id;
 }
