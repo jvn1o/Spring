37236e740eee6795f800c7ecb61d35f2b3a6a00d
jvn1o <junok0987@naver.com>
2025-03-20 17:49:42 +0900

    250320

---
+++ demo/src/main/java/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hellospring.repository;
+
+import hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
