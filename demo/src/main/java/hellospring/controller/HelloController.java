3377be2ab64e1bea2abb73b51706cafe7c4314b7
jvn1o <junok0987@naver.com>
2025-03-20 17:08:58 +0900

    250320

--- demo/src/main/java/hellospring/controller/HelloController.java
+++ demo/src/main/java/hellospring/controller/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
 
     @GetMapping("hello-string")
     @ResponseBody
-    public String helloString(@RequestParam String name) {
+    public String helloString(@RequestParam("name") String name) {
         return "hello " + name;
     }
 
