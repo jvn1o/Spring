9e04826a61bfa7705d450f70fbf9f4b24b153fd2
jvn1o <junok0987@naver.com>
2025-03-21 18:30:02 +0900

    250321

--- demo/src/main/java/hellospring/service/MemberService.java
+++ demo/src/main/java/hellospring/service/MemberService.java
@@ -3,10 +3,12 @@ package hellospring.service;
 import hellospring.domain.Member;
 import hellospring.repository.MemberRepository;
 import hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
