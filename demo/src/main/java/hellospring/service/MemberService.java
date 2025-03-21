1232d135d1717126f0e047bd8089b4012c0dbe7c
jvn1o <junok0987@naver.com>
2025-03-21 15:22:57 +0900

    250321

---
+++ demo/src/main/java/hellospring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hellospring.service;
+
+import hellospring.domain.Member;
+import hellospring.repository.MemberRepository;
+import hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 중복 ID 금지 -> ctrl + alt + v
+        /* 권장하지 않는 방법
+        Optional<Member> result = memberRepository.findById(member.getId());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }); */
+        validateDuplicateMember(member); // ctrl + alt + m 으로 메소드 만들 수 있다.
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findById(member.getId())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
